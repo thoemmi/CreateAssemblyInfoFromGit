367a8ffae47ee59903443540db36323dcbab0389
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-05-31 02:20:54 +0200

    initial commit

---
+++ src/CreateAssemblyInfoFromGit/CreateAssemblyInfoFromGit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CreateAssemblyInfoFromGit {
+    public class CreateAssemblyInfoFromGit : Task {
+        [Required]
+        public ITaskItem Repository { get; set; }
+
+        [Required]
+        public ITaskItem AssemblyInfoPath { get; set; }
+
+        public override bool Execute() {
+
+
+            var version = GitHelper.GetVersion(Repository.ItemSpec);
+
+            if (File.Exists(AssemblyInfoPath.ItemSpec)) {
+                var oldContent = File.ReadAllText(AssemblyInfoPath.ItemSpec);
+                if (Regex.IsMatch(oldContent, @"AssemblyInformationalVersion\(" + version + @"\)")) {
+                    Log.LogMessage(MessageImportance.Low, Repository.ItemSpec + " is up-to-date.");
+                    return true;
+                }
+            }
+
+            var fileVersion = version.Replace("-beta", ".");
+            var content = String.Format(@"// Generated: {0} (UTC)
+// Warning: This is generated code! Don't touch as it will be overridden by the build process.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion(""{1}"")]
+[assembly: AssemblyFileVersion(""{1}"")]
+[assembly: AssemblyInformationalVersion(""{2}"")]", DateTime.UtcNow, fileVersion, version);
+            File.WriteAllText(AssemblyInfoPath.ItemSpec, content);
+
+            return true;
+        }
+    }
+}
---
+++ src/CreateAssemblyInfoFromGit/GitHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using LibGit2Sharp;
+
+namespace CreateAssemblyInfoFromGit {
+    internal static class GitHelper {
+        public static string GetVersion(string path) {
+            using (var repository = new Repository(GetGitRepositoryPath(path))) {
+                var allTags = repository.Tags.ToList();
+
+                var build = 0;
+                string version = null;
+                foreach (var commit in repository.Head.Commits) {
+                    var tags = allTags.Where(t => t.IsAnnotated && t.Target == commit).ToList();
+                    if (build == 0 && TryGetVersion(tags, @"v\-?", out version)) {
+                        break;
+                    }
+                    if (TryGetVersion(tags, @"vNext\-?", out version)) {
+                        break;
+                    }
+                    if (TryGetVersion(tags, @"v\-?", out version)) {
+                        break;
+                    }
+
+                    build++;
+                }
+
+                if (build != 0) {
+                    version += "-beta" + build;
+                }
+                return version;
+            }
+        }
+
+        public static string GetGitRepositoryPath(string path) {
+            if (path == null) throw new ArgumentNullException("path");
+
+            //If we are passed a .git directory, just return it straightaway
+            var pathDirectoryInfo = new DirectoryInfo(path);
+            if (pathDirectoryInfo.Name == ".git") {
+                return path;
+            }
+
+            if (!pathDirectoryInfo.Exists) {
+                return Path.Combine(path, ".git");
+            }
+
+            DirectoryInfo checkIn = pathDirectoryInfo;
+
+            while (checkIn != null) {
+                var pathToTest = Path.Combine(checkIn.FullName, ".git");
+                if (Directory.Exists(pathToTest)) {
+                    return pathToTest;
+                } else {
+                    checkIn = checkIn.Parent;
+                }
+            }
+
+            // This is not good, it relies on the rest of the code being ok
+            // with getting a non-git repo dir
+            return Path.Combine(path, ".git");
+        }
+
+        private static bool TryGetVersion(IEnumerable<Tag> tags, string prefix, out string version) {
+            string pattern = @"^" + prefix + @"(?<minormajor>\d+\.\d+)(?<patch>\.\d+)?$";
+            foreach (var tag in tags) {
+                var match = Regex.Match(tag.Name, pattern);
+                if (match.Success) {
+                    var minormajor = match.Groups["minormajor"].Value;
+                    var patch = match.Groups["patch"].Value;
+                    version = minormajor + (String.IsNullOrEmpty(patch) ? ".0" : patch);
+                    return true;
+                }
+            }
+            version = default(string);
+            return false;
+        }
+    }
+}
